bf6f017d91b97f11f2b62c69f9df0ff261b88cc9
Cristian Vaquero <cristianvaquerodcv@gmail.com>
2022-03-13 09:19:50 -0300

    Subiendo desafio 3

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -8,34 +8,28 @@ import NavDropdown from 'react-bootstrap/NavDropdown'
 function NavBar() {
   return (
 
-    <Navbar bg="light" expand="lg">
-      <Container fluid>
-        <Navbar.Brand href="#">La Tienda</Navbar.Brand>
-        <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse id="navbarScroll">
-          <Nav
-            className="me-auto my-2 my-lg-0"
-            style={{ maxHeight: '100px' }}
-            navbarScroll
-          >
-            <Nav.Link href="#action1">Home</Nav.Link>
-            <Nav.Link href="#action2">Tienda</Nav.Link>
-            <NavDropdown title="Tienda" id="navbarScrollingDropdown">
-              <NavDropdown.Item href="#action3">Remeras</NavDropdown.Item>
-              <NavDropdown.Item href="#action4">Camisas</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action5">
-                Camperas
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#" disabled>
-              Carrito
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-   </Navbar>
-
+    <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+  <Container>
+  <Navbar.Brand href="#home">Shop React</Navbar.Brand>
+  <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+  <Navbar.Collapse id="responsive-navbar-nav">
+    <Nav className="me-auto">
+      <Nav.Link href="#staff">Staff</Nav.Link>
+      <Nav.Link href="#contacto">Contact</Nav.Link>
+      <NavDropdown title="Category" id="collasible-nav-dropdown">
+        <NavDropdown.Item href="#action/3.1">Category 1</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.2">Category 2</NavDropdown.Item>
+        <NavDropdown.Item href="#action/3.3">Category 3</NavDropdown.Item>
+        <NavDropdown.Divider />
+        <NavDropdown.Item href="#action/3.4">Other category</NavDropdown.Item>
+      </NavDropdown>
+    </Nav>
+    <Nav>
+      <Nav.Link href="#carrito">Shop</Nav.Link>
+    </Nav>
+  </Navbar.Collapse>
+  </Container>
+</Navbar>
 
   )
 }
